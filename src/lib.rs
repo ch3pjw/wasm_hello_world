f05b66a0b073a7c4cc77b7eded85c499752fa507
Paul Weaver <paul@concertdaw.co.uk>
2021-03-03 11:20:24 +0000

    Establish a websocket and close it

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
 mod utils;
 
-use wasm_bindgen::prelude::*;
+use {
+    wasm_bindgen::prelude::*,
+    wasm_bindgen_futures::spawn_local,
+
+    futures::{stream::StreamExt},
+    pharos::*,
+    ws_stream_wasm::*,
+};
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -15,5 +22,13 @@ extern {
 
 #[wasm_bindgen]
 pub fn greet() {
-    alert("Hello, wasm-hello-world!");
+    let program = async {
+        let (mut ws, _wsio) = WsMeta::connect("wss://echo.websocket.org", None).await.expect_throw( "failed :-(");
+        let mut evts = ws.observe(ObserveConfig::default()).await.expect_throw("observe died");
+        ws.close().await;
+        assert!(evts.next().await.unwrap_throw().is_closing());
+        assert!(evts.next().await.unwrap_throw().is_closed());
+        alert("Hello, wasm-hello-world! I closed a websocket");
+    };
+    spawn_local(program);
 }
