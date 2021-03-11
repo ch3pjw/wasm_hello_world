b7742bd0f9935e1449a6126f7c76ae1a3e7370b5
Paul Weaver <paul@concertdaw.co.uk>
2021-03-11 11:33:01 +0000

    Finally get a websocket handshake to work

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,8 @@
 use {
+    crypto::{
+        digest::Digest,
+        sha1::Sha1,
+    },
     hyper::{
         Body,
         Request,
@@ -14,8 +18,8 @@ use {
     std::{
         convert::Infallible,
         net::SocketAddr,
-        str,
     },
+    tokio::io::AsyncReadExt
 };
 
 #[tokio::main]
@@ -75,10 +79,20 @@ fn err_resp(code: StatusCode) -> Result<Response<Body>, http::Error> {
 }
 
 fn handle_ws(mut req: Request<Body>) -> Result<Response<Body>, http::Error> {
+    if req.headers().get(header::UPGRADE) != Some(&hv("websocket")) ||
+            req.headers().get("sec-websocket-version") != Some(&hv("13")) {
+        return err_resp(StatusCode::BAD_REQUEST);
+    }
     if req.uri().path() != "/" {
         error!("Bad websocket path: {}", req.uri().path());
         return err_resp(StatusCode::NOT_FOUND);
     }
+
+    let sec_websocket_accept_header = match req.headers().get("Sec-WebSocket-Key") {
+        None => { return err_resp(StatusCode::BAD_REQUEST); }
+        Some(key) => mk_accept_header(key.as_bytes())
+    };
+
     tokio::task::spawn(async move {
         match hyper::upgrade::on(&mut req).await {
             Ok(upgraded) => {
@@ -89,17 +103,60 @@ fn handle_ws(mut req: Request<Body>) -> Result<Response<Body>, http::Error> {
             Err(e) => error!("upgrade error: {}", e)
         }
     });
-    Response::builder()
-        .status(StatusCode::SWITCHING_PROTOCOLS)
-        .header(header::UPGRADE, header::HeaderValue::from_static("blah"))
-        .body(Body::empty())
+
+    let mut resp = Response::new(Body::empty());
+    *resp.status_mut() = StatusCode::SWITCHING_PROTOCOLS;
+    let headers = resp.headers_mut();
+    headers.insert(header::UPGRADE, hv("websocket"));
+    headers.insert(header::CONNECTION, hv("Upgrade"));
+    headers.insert(
+        "Sec-WebSocket-Accept",
+        header::HeaderValue::from_str(&sec_websocket_accept_header).expect("this should never fail")
+    );
+    headers.insert("Sec-WebSocket-Protocol", hv("foo"));
+    return Ok(resp);
+}
+
+fn hv(string: &'static str) -> header::HeaderValue {
+    header::HeaderValue::from_static(string)
 }
 
 
 async fn websocket_dialogue(mut upgraded: hyper::upgrade::Upgraded) -> Result<(), hyper::Error> {
-    let mut buf = [0u8; 6];
     loop {
-        upgraded.read_exact(&mut buf).await?;
-        info!("Server received {:?}", str::from_utf8(&buf));
+        let mut buf = [0u8; 1024];
+        let bytes_read = match upgraded.read(&mut buf).await {
+            Ok(bytes_read) => {
+                if bytes_read == 0 /* EOF */ { return Ok(()) };
+                bytes_read
+            },
+            Err(e) => {
+                error!("Websocket error {:?}", e);
+                todo!("translate e into hyper::Error or change our error type everywhere")
+            }
+        };
+        info!("Server received {:?}", &buf[0..bytes_read]);
+    }
+}
+
+
+fn mk_accept_header(key_header: &[u8]) -> String {
+    let mut hasher = Sha1::new();
+    hasher.input(key_header);
+    // A magic UUID that makes it go (https://en.wikipedia.org/wiki/WebSocket)
+    hasher.input(b"258EAFA5-E914-47DA-95CA-C5AB0DC85B11");
+    let mut hashed = vec![0u8; hasher.output_bytes()];
+    hasher.result(&mut hashed);
+    base64::encode(hashed)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_accept_header() {
+        // Example taken from https://en.wikipedia.org/wiki/WebSocket
+        assert_eq!(mk_accept_header(b"x3JJHMbDL1EzLkh9GBhXDw=="), "HSmrc0sMlYUkAGmm5OPpG2HaGWk=")
     }
 }
