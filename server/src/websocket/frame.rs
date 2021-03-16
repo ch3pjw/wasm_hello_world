4827b70476f48fd95476f1654a620ed1fbf7b4db
Paul Weaver <paul@concertdaw.co.uk>
2021-03-16 22:16:16 +0000

    Add handy diagram from RFC6455

--- server/src/websocket/frame.rs
+++ server/src/websocket/frame.rs
@@ -35,6 +35,27 @@ enum_from_primitive! {
     }
 }
 
+//    Base Frame Protocol from RFC6455:
+//
+//    0                   1                   2                   3
+//    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+//   +-+-+-+-+-------+-+-------------+-------------------------------+
+//   |F|R|R|R| opcode|M| Payload len |    Extended payload length    |
+//   |I|S|S|S|  (4)  |A|     (7)     |             (16/64)           |
+//   |N|V|V|V|       |S|             |   (if payload len==126/127)   |
+//   | |1|2|3|       |K|             |                               |
+//   +-+-+-+-+-------+-+-------------+ - - - - - - - - - - - - - - - +
+//   |     Extended payload length continued, if payload len == 127  |
+//   + - - - - - - - - - - - - - - - +-------------------------------+
+//   |                               |Masking-key, if MASK set to 1  |
+//   +-------------------------------+-------------------------------+
+//   | Masking-key (continued)       |          Payload Data         |
+//   +-------------------------------- - - - - - - - - - - - - - - - +
+//   :                     Payload Data continued ...                :
+//   + - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +
+//   |                     Payload Data continued ...                |
+//   +---------------------------------------------------------------+
+
 impl Frame {
     // Currently trivial, but might want different error handling:
     pub fn parse(input: &[u8]) -> IResult<&[u8], Frame> {
