67727e3a2c07d712a767edce24f91a5eac7f1fde
Paul Weaver <paul@concertdaw.co.uk>
2021-03-26 17:42:46 +0000

    Move resources out of main.rs

--- server/src/main.rs
+++ server/src/main.rs
@@ -11,7 +11,6 @@ use {
     },
     hyper::{
         Body,
-        body::Bytes,
         Request,
         Response,
         Server,
@@ -34,6 +33,7 @@ use {
 };
 
 mod service;
+mod resources;
 
 use crate::service::ConnectionHandler;
 
@@ -193,42 +193,19 @@ fn handle_request(req: Request<Body>, tx: mpsc::UnboundedSender<AppCmd>) -> Resu
     }
 }
 
-const CLIENT_HTML_TEMPLATE: &[u8] = include_bytes!("../templates/index.html.template");
-
-fn generate_client_html(host: &[u8]) -> Bytes {
-    // TODO: can we do this up front?
-    let mut head = CLIENT_HTML_TEMPLATE;
-    let mut tail = &CLIENT_HTML_TEMPLATE[0..0];
-    for (i, pair) in CLIENT_HTML_TEMPLATE.windows(2).enumerate() {
-        if pair == b"{}" {
-            head = &CLIENT_HTML_TEMPLATE[..i];
-            tail = &CLIENT_HTML_TEMPLATE[i+2..];
-            break
-        }
-    }
-    Bytes::from([head, host, tail].concat())
-}
-
-const CLIENT_JS: Bytes = Bytes::from_static(
-    include_bytes!("../../client/static/wasm_hello_world.js")
-);
-const CLIENT_WASM: Bytes = Bytes::from_static(
-    include_bytes!("../../client/static/wasm_hello_world_bg.wasm")
-);
-
 fn handle_get(req: Request<Body>) -> Result<Response<Body>, http::Error> {
     let b = Response::builder();
     let x = match req.uri().path() {
         "/" | "/index.html" => {
             if let Some(host) = req.headers().get("host") {
-                Some(("text/html", generate_client_html(host.as_bytes())))
+                Some(("text/html", resources::generate_client_html(host.as_bytes())))
             } else {
                 warn!("Request missing host header!");
                 None
             }
         },
-        "/wasm_hello_world.js" => Some(("application/javascript", CLIENT_JS)),
-        "/wasm_hello_world_bg.wasm" => Some(("application/wasm", CLIENT_WASM)),
+        "/wasm_hello_world.js" => Some(("application/javascript", resources::CLIENT_JS)),
+        "/wasm_hello_world_bg.wasm" => Some(("application/wasm", resources::CLIENT_WASM)),
         path => {
             warn!("Requested missing path: {}", path);
             None
