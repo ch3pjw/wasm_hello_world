d1174ba9479d66a55ec627a4efe53e52ed6f4d26
Paul Weaver <paul@concertdaw.co.uk>
2021-03-28 03:27:43 +0100

    Make template at compile time
    by reading the file in a procedural macro and spitting out a format!()
    invocation. (Oh my word it took me AGES to figure out that format!() was
    super unhappy with all the braces in the template file :joy:)

--- server/macros/src/lib.rs
+++ server/macros/src/lib.rs
@@ -1,13 +1,30 @@
 use {
     proc_macro::{self, TokenStream},
     quote::quote,
-    syn::parse_macro_input,
+    syn::{
+        Expr, ExprLit,
+        Lit, LitStr,
+        parse::Parser,
+        punctuated::Punctuated,
+        token::Comma,
+    },
+};
+
+use {
+    std::env,
+    std::fs::read_to_string,
 };
 
 #[proc_macro]
-pub fn hello(input: TokenStream) -> TokenStream {
-    let output = quote! {
-        println!("hello world")
+pub fn template(input: TokenStream) -> TokenStream {
+    let args = Punctuated::<Expr, Comma>::parse_separated_nonempty.parse(input).unwrap();
+    let path_lit = match &args[0] {
+        Expr::Lit(ExprLit{ lit: Lit::Str(s), .. }) => s,
+        _ => panic!("first argument must be a path")
     };
+    let args = &args.iter().collect::<Vec<&Expr>>()[1..];
+    let path = env::current_dir().unwrap().join("src/").join(path_lit.value());
+    let s_lit = LitStr::new(&read_to_string(path).unwrap(), path_lit.span());
+    let output = quote! { format!(#s_lit, #(#args),*) };
     output.into()
 }
